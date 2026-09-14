6f67adb5ab701b6f813148ca7b2da534913f8682
Jay3-yy <173592055+jay3-yy@users.noreply.github.com>
2026-09-14 23:39:40 +0800

    Restore standard now playing navigation

--- app/src/main/java/com/android/purebilibili/navigation/AppNavigation.kt
+++ app/src/main/java/com/android/purebilibili/navigation/AppNavigation.kt
@@ -3895,7 +3895,7 @@ fun AppNavigation(
                         if ((navigation3BackStack.lastOrNull() as? BiliPaiNavKey.VideoDetail)
                                 ?.entrySource == VideoDetailEntrySource.AUDIO_NOW_PLAYING_BAR
                         ) {
-                            sharedVideoCardTransitionEnabled
+                            false
                         } else {
                             com.android.purebilibili.navigation3.resolveVideoCardTransitionEnabledForSource(
                                 cardTransitionEnabled = sharedVideoCardTransitionEnabled,
@@ -4082,7 +4082,7 @@ fun AppNavigation(
                                         liftAboveBottomBar = false,
                                         consumeNavigationBarsPadding = false,
                                         dockHosted = isBottomBarFloating,
-                                        videoDetailMorphEnabled = !audioNowPlayingBarOpensAudioMode,
+                                        videoDetailMorphEnabled = false,
                                         onBoundsChanged = { audioNowPlayingBarBounds = it },
                                         dockMergeProgress = dockMergeProgress,
                                         surfaceMergeProgress = surfaceMergeProgress,
@@ -4251,7 +4251,7 @@ fun AppNavigation(
                     liquidGlassTuning = liquidGlassRenderConfig.tuning,
                     liftAboveBottomBar = false,
                     consumeNavigationBarsPadding = true,
-                    videoDetailMorphEnabled = !audioNowPlayingBarOpensAudioMode,
+                    videoDetailMorphEnabled = false,
                     onBoundsChanged = { audioNowPlayingBarBounds = it },
                     modifier = Modifier
                         .align(Alignment.BottomCenter)
